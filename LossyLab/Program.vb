36241b5076a278fa463f97815450f97aa5a6243d
Syed Muhammad Ali Asher <127295261+Asher-1971@users.noreply.github.com>
2024-02-04 13:39:28 +0500

    Update Program.vb

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -2,12 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("did this on visual stuidio desktop ")
-
+      
 
     End Sub
 End Module
