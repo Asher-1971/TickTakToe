a4943c8474da7112aae0fb39c97140a4f51c03ae
Syed Muhammad Ali Asher <127295261+Asher-1971@users.noreply.github.com>
2024-02-04 12:59:43 +0500

    Initial commit

---
+++ LossyLab/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("did this on vs code")
+        Console.WriteLine("")
+        Console.WriteLine("did this on vs code")
+        Console.WriteLine("did this on visual stuidio desktop ")
+
+
+    End Sub
+End Module
